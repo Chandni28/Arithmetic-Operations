25f4ecfc29bdeced16dcc91f78c11e8e5ab509c8
Chandni28 <zhan.nsk@gmail.com>
2017-06-27 12:50:11 +0530

    Add files via upload

---
+++ Check.java
@@ -0,0 +1,26 @@
+package test.java.payilagam;
+
+public class Check {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Check chk=new Check();
+ chk.Check();
+ String name="Yoghitha";
+ int age=2;
+	
+ 
+ public Check
+ {
+	 System.out.println("Program to display Name and Age");
+	 		
+ }
+ 
+ public Check(String string, int x)
+ {
+	 
+	 System.out.println(" "+name +" " +age);
+ }
+	}
+
+}
